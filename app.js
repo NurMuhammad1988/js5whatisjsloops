e87d12654a7c275ac8733a50c6c73cb234baadad
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-02-13 16:46:54 +0500

    djklsdhsajdhasdjhad

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+
+// let i = 0;
+// while (i < 22) {
+//     alert(i);
+//     i++;
+// }
+
+
+// let i = 11;
+// while (i){
+//     alert(i);
+//     i--;
+// }
+
+
+// let i = 1;
+// while (i <= 10) {
+//     document.write('<h1>' + i + '<br>');
+//     i++;
+// }
+
+
+// let num = +prompt('Raqam yozing');
+// let power = +prompt('Darajasini yozing');
+// let res = 1;
+// let i = 1;
+
+// while(i <= power){
+//     res = res * num;
+//     i++;
+// }
+
+// document.write(res);
+
+
+for (let i = 1; i < 7; i++) { 
+    alert(i);
+  }
+
